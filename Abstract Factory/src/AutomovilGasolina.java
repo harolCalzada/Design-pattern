850cd81ea322a2edd585c2620da0457202a4122b
haro28 <harol.calzada@gmail.com>
2016-03-23 00:29:55 -0500

    Agregando el primer patron Abstract Factory

---
+++ Abstract Factory/src/AutomovilGasolina.java
@@ -0,0 +1,10 @@
+
+public class AutomovilGasolina extends Automovil{
+	public AutomovilGasolina(String modelo, String color, int potencia , double espacio){
+		super(modelo, color ,potencia, espacio);
+	}
+	public void mostrarCaracteristicas(){
+		System.out.println("Automovil de gasolina modelo: " + modelo + " de color: " + color +
+						   "de potencia: " + potencia + " de espacio: " + espacio);
+	}
+}
---
+++ Abstract Factory/src/ScooterGasolina.java
@@ -0,0 +1,10 @@
+
+public class ScooterGasolina extends Scooter{
+	
+	public ScooterGasolina(String modelo , String color, int potencia){
+		super(modelo, color, potencia);
+	}
+	public void mostrarCaracteristicas(){
+		System.out.println("Scooter de gasolina modelo: " + modelo + " de color: " + color + " de potencia: " + potencia);
+	}
+}
